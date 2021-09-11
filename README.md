3ef428e3dd35831069f85266ad5bf0b2d88ef9af
phillzou <zoumingyuan@bytedance.com>
2021-09-11 17:13:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TypeScript Challenge Answer
+
+记录一下 [TypeScript Challenge](https://github.com/type-challenges/type-challenges) 里的答案。
+
---
+++ medium/108-trim.ts
@@ -0,0 +1,48 @@
+/*
+  108 - Trim
+  -------
+  by Anthony Fu (@antfu) #medium #template-literal
+  
+  ### Question
+  
+  Implement `Trim<T>` which takes an exact string type and returns a new string with the whitespace from both ends removed.
+  
+  For example
+  
+  ```ts
+  type trimed = Trim<'  Hello World  '> // expected to be 'Hello World'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/108
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+type TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer F}` ? TrimLeft<F> : S;
+type TrimRight<S extends string> = S extends `${infer F}${' ' | '\n' | '\t'}` ? TrimRight<F> : S;
+
+type Trim<S extends string> = TrimRight<TrimLeft<S>>;
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/108/answer
+  > View solutions: https://tsch.js.org/108/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
