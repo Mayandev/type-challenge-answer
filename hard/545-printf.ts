395ec3cd2a436b26c87f3fe6c7b5e73740a665ae
mayandev <phillzou@gmail.com>
2021-09-15 15:47:54 +0800

    fix: fix some hard question

---
+++ hard/545-printf.ts
@@ -0,0 +1,48 @@
+/*
+  545 - printf
+  -------
+  by null (@BestMaster-YS) #hard #template-literal
+  
+  ### Question
+  
+  Implement `Format<T extends string>` generic.
+  
+  For example,
+  
+  ```ts
+  type FormatCase1 = Format<"%sabc"> // FormatCase1 : string => string
+  type FormatCase2 = Format<"%s%dabc"> // FormatCase2 : string => number => string
+  type FormatCase3 = Format<"sdabc"> // FormatCase3 :  string
+  type FormatCase4 = Format<"sd%abc"> // FormatCase4 :  string
+  ```
+  
+  > View on GitHub: https://tsch.js.org/545
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+// your answers
+type Format<T extends string> = T extends `${infer N}%${infer K}${infer L}` ? K extends 's' ? (arg: string)=> Format<L> : K extends 'd' ? (arg: number)=> Format<L> : never : string
+
+
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Format<'abc'>, string>>,
+  Expect<Equal<Format<'a%sbc'>, (s1: string) => string>>,
+  Expect<Equal<Format<'a%dbc'>, (d1: number) => string>>,
+  Expect<Equal<Format<'a%dbc%s'>, (d1: number) => (s1: string) => string>>
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/545/answer
+  > View solutions: https://tsch.js.org/545/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
